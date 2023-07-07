2e052d3bf7d319bc17cd79adfb0735812263aec4
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-07-07 10:09:35 +0530

    Create Palindrome_recurssion.cpp

---
+++ Palindrome_recurssion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string palindrome(string str,int i,int j) {
+  if(i>=j){
+    return "Palindrome";
+  }
+  if(str[i]==str[j]){
+    return palindrome(str,i+1,j-1);
+  }
+  return "Not Palindrome";
+  
+}
+
+int main() {
+  string str;
+  cout<<"Enter the string: ";
+  cin >> str;
+  int len;
+  len=str.length();
+  cout<<palindrome(str,0,len-1);
+  return 0;
+}
